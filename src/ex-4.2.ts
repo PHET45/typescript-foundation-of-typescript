964d0f2487d6de34bb598adb16c01c9fd0efc978
PHET45 <phnjk2000@gmail.com>
2025-07-30 16:31:33 +0700

    Update processData function to handle string and number types, returning appropriate responses based on input type

--- src/ex-4.2.ts
+++ src/ex-4.2.ts
@@ -1,8 +1,14 @@
-function processData(data: unknown): string {
-  return data.toUpperCase();
+function processData(data: string | number): string {
+  if (typeof data === "string") {
+    return data.toUpperCase();
+  } else {
+    return "Not a string";
+  }
 }
 
 console.log(processData("hello")); // ควรได้ "HELLO"
 console.log(processData(123)); // ควรได้ "Not a string"
 
-// Error ที่เจอคือ
+// Error ที่เจอคือ TSError: ⨯ Unable to compile TypeScript:src/ex-4.2.ts(2,10): error TS18046: 'data' is of type 'unknown'. หมายความว่า มีการใช้ data (ซึ่งมีชนิดเป็น unknown) โดยไม่ตรวจสอบชนิดของมันก่อน
+
+
